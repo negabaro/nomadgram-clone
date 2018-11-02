7879cd5c0fe66c751e911f2f4da1c9fc5de55207
negabaro kim <negabaro@naver.com>
2018-11-02 18:29:00 +0000

    1.39-연습용url커맨트

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -1,10 +1,24 @@
-from django.urls import path
+#from django.urls import path
+from django.conf.urls import url
 from . import views
 
 app_name = "images"
 urlpatterns = [
-    path("all/", view=views.ListAllImages.as_view(), name="all_imaes"),
-    path("comments/", view=views.ListAllComments.as_view(), name="all_comments"),
-    path("likes/", view=views.ListAllLikes.as_view(), name="all_likes"),
-    path("feed/",view=views.Feed.as_view(), name="feed"),
+    url(
+      regex=r'^$',
+      view=views.Feed.as_view(),
+      name="feed"  
+    ),
+  
 ]
+#url사용할때는 regex필수였다
+    
+
+#app_name = "images"
+#urlpatterns = [
+#    path("all/", view=views.ListAllImages.as_view(), name="all_imaes"),
+#    path("comments/", view=views.ListAllComments.as_view(), name="all_comments"),
+#    path("likes/", view=views.ListAllLikes.as_view(), name="all_likes"),
+#    path("feed/",view=views.Feed.as_view(), name="feed"),
+#]
+#연습용이었으므로 삭제 
