1cf2133ccaea2e89ce9c7a884d74a4ce1c71bdf4
negabaro kim <negabaro@naver.com>
2018-11-07 19:46:47 +0000

    1-58 Creating Notification Models

--- config/settings/base.py
+++ config/settings/base.py
@@ -72,6 +72,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'nomadgram.users.apps.UsersAppConfig',
     'nomadgram.images.apps.ImagesConfig',
+    'nomadgram.notifications.apps.NotificationsConfig'  # notifications app
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
